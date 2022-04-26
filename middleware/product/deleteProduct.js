f3edb500170229f2b158a3679d4571d87d742de0
Drexler Nandor <drexler.nandor@ma-coding.com>
2022-04-26 20:21:49 +0200

    product delete

--- middleware/product/deleteProduct.js
+++ middleware/product/deleteProduct.js
@@ -1,16 +1,30 @@
 //Checks if a products's id in the database matches with :productId, If the product doesn't have any measurement's then deletes it.
 //Redirects to / after removal
+const requireOption = require("../requireOption");
+
 module.exports = function (objectrepository) {
+    const MeasurementModel = requireOption(objectrepository, "MeasurementModel");
+
     return function (req, res, next) {
         console.log("Requested ID:" + req.params.productid);
-        if(typeof res.locals.product === "undefined"){
+        if (typeof res.locals.product === "undefined") {
             return next();
         }
-        res.locals.product.remove((err) => {
-            if(err){
+
+        MeasurementModel.count({ _measuredProduct: { _id: res.locals.product._id } }, function (err, count) {
+
+            if (err) {
                 return next(err);
             }
-        })
-        return res.redirect("/products");
-    };
+
+            if (count < 1) {
+                res.locals.product.remove((err) => {
+                    if (err) {
+                        return next(err);
+                    }
+                });
+            }
+            return res.redirect("/products");
+        });
+    }
 };
