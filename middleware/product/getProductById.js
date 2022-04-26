36f2075a8280e01b24222623a0f200062f59f65e
Drexler Nandor <drexler.nandor@ma-coding.com>
2022-04-26 20:21:25 +0200

    Product dangling delete fix

--- middleware/product/getProductById.js
+++ middleware/product/getProductById.js
@@ -3,15 +3,15 @@
 const requireOption = require("../requireOption");
 
 module.exports = function (objectrepository) {
-    const ProductModel = requireOption(objectrepository,"ProductModel");
+    const ProductModel = requireOption(objectrepository, "ProductModel");
 
     return function (req, res, next) {
         console.log("Requested ID:" + req.params.productid);
-        ProductModel.findOne({_id:req.params.productid}, (err, product) => {
-            if (err || ! product) {
+
+        ProductModel.findOne({ _id: req.params.productid }, (err, product) => {
+            if (err || !product) {
                 return next(err);
             }
-
             res.locals.product = product;
             return next();
         });
